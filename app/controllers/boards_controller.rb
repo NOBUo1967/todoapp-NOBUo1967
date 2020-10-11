bfbfb7b4ddedb90b92656a270f2c16783eb3533d
Shin H <nobunegi13@gmail.com>
2020-10-11 12:48:04 +0900

    board保存機能作成、validation、flash_message

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,19 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to board_path(@board),notice: 'save it'
+    else
+      flash.now[:error] = 'failed to save it'
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:name, :description)
+  end
 end
