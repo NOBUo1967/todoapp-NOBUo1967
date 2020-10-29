749c879c590c6692f8747c8462a909f90cab70a0
Shin H <nobunegi13@gmail.com>
2020-10-29 15:39:39 +0900

    board詳細ページからtask詳細ページへの遷移機能を作成。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
 class TasksController < ApplicationController
 
+  def show
+    board = Board.find(params[:board_id])
+    @task = board.tasks.find(params[:id])
+  end
+
   def new
     board = Board.find(params[:board_id])
     @task = board.tasks.build
@@ -7,7 +12,7 @@ class TasksController < ApplicationController
   
   def create
     board = Board.find(params[:board_id])
-    @task = board.tasks.build(task_params)
+    @task = board.tasks.build(task_params.merge(user_id: current_user.id))
     if @task.save
       redirect_to board_path(board), notice: 'Task was added successfully!'
     else
@@ -19,6 +24,6 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :description, :term, :eyecatch).merge(user_id: current_user.id)
+    params.require(:task).permit(:name, :description, :term, :eyecatch)
   end
 end
