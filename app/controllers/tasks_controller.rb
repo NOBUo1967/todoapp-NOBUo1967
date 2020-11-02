6605ebbb810a0b1a20aaa1748cc155f44c9443df
Shin H <nobunegi13@gmail.com>
2020-11-02 11:46:04 +0900

    task編集機能を追加した。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,48 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
 
   def show
-    board = Board.find(params[:board_id])
-    @task = board.tasks.find(params[:id])
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
   end
 
   def new
-    board = Board.find(params[:board_id])
-    @task = board.tasks.build
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.build
   end
   
   def create
-    board = Board.find(params[:board_id])
-    @task = board.tasks.build(task_params.merge(user_id: current_user.id))
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.build(task_params.merge(user_id: current_user.id))
     if @task.save
-      redirect_to board_path(board), notice: 'Task was added successfully!'
+      redirect_to board_path(@board), notice: 'Task was added successfully!'
     else
       flash.now[:error] = 'Failed to add the task'
       render :new
     end
+  end
+
+  def edit
+    @board = current_user.boards.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
+  end
+
+  def update
+    @board = current_user.boards.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to board_task_path(@board, @task), notice: 'Update the task'
+    else
+      flash.now[:error] = 'Failed to update'
+      render :edit
+    end
+  end
+
+  def destroy
+    board = current_user.boards.find(params[:id])
+    @task = board.tasks.find(params[:id])
+    @task.destroy!
+    redirect_to root_path, notice: 'Board has been successfully deleted.'
   end
 
 
